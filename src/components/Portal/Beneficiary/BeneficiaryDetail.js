e27500b4011bb6881d1402ef06f921ab4e332e4b
Rizky Agung Saputro <co.terbang@gmail.com>
2018-03-18 17:00:29 +0700

    Beneficiaries Detail

--- src/components/Portal/Beneficiary/BeneficiaryDetail.js
+++ src/components/Portal/Beneficiary/BeneficiaryDetail.js
@@ -50,8 +50,214 @@ class BeneficiaryDetail extends React.Component {
                                                 value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Full_Name__c : ''}
                                                 disabled="true" />
                                         </div>
+                                        <div className="form-group">
+                                            <label>ID Type</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].ID_Type__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>ID Number</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].ID_Number__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Gender</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Gender__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Current School</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Current_School__r.Name : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Nationality</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Nationality__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Religion</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Religion__c : ''}
+                                                disabled="true" />
+                                        </div>
                                     </div>
+                                <div className="col-sm-6">
+                                    <div className="form-group">
+                                        <label>Relationship to Applicant</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Relationship_to_Applicant__c : ''}
+                                            disabled="true" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Application Date</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Application_Date__c : ''}
+                                            disabled="true" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Date of Birth</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Full_Name__c : ''}
+                                            disabled="true" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Age at Application</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Age_at_Application__c : ''}
+                                            disabled="true" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Race</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Race__c : ''}
+                                            disabled="true" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Marital Status</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Marital_Status__c : ''}
+                                            disabled="true" />
+                                    </div>
+                                </div>                                
                                 </div>
+                                <div className="x_title">
+                                    Beneficiary Information
+                                    <div className="clearfix" />
+                                </div>
+                                <div className="x_content" style={{ display: 'block' }} >
+                                    <div className="col-sm-6">
+                                        <div className="form-group">
+                                            <label>Applying to</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Applying_to__r.Name : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Referred From</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Referred_From__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Referred Date</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Referred_Date__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Referring Reason</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Referring_Reason__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <div className="form-group">
+                                            <label>Application Type</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Application_Type__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Status</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Application_Status__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Current Level</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Current_Level__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Stream</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Stream__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                    </div>                             
+                                </div>
+                                <div className="x_title">
+                                    Contact Details
+                                    <div className="clearfix" />
+                                </div>
+                                <div className="x_content" style={{ display: 'block' }} >
+                                    <div className="col-sm-6">
+                                        <div className="form-group">
+                                            <label>Email Address</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Email_Address__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <div className="form-group">
+                                            <label>Home Phone</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Home_Phone__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Mobile Phone</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={this.state.BeneDetail.length > 0 ? this.state.BeneDetail[0].Mobile_Phone__c : ''}
+                                                disabled="true" />
+                                        </div>
+                                    </div>
+                                </div>                                 
                             </div>
                         </div>
                     </div>
